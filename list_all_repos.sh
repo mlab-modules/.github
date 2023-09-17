2ad9013afb9fec117bc568c1ee1bd53fcc0e42d8
Roman Dvorak <romandvorak@mlab.cz>
2023-09-17 11:42:02 +0200

    extend number of requests

--- list_all_repos.sh
+++ list_all_repos.sh
@@ -3,14 +3,6 @@
 
 echo "Vytvorit seznam vsech repozitaru"
 
-#repos=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/orgs/mlab-modules/repos?page=1 | jq '. | map(.ssh_url) | @sh'  | tr -d '"' | tr -d "\'")
-#repos=$repos$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/orgs/mlab-modules/repos?page=2 | jq '. | map(.ssh_url) | @sh'  | tr -d '"' | tr -d "\'")
-#repos=$repos$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/orgs/mlab-modules/repos?page=3 | jq '. | map(.ssh_url) | @sh'  | tr -d '"' | tr -d "\'")
-#repos=$repos$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/orgs/mlab-modules/repos?page=4 | jq '. | map(.ssh_url) | @sh'  | tr -d '"' | tr -d "\'")
-#repos=$repos$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/orgs/mlab-modules/repos?page=5 | jq '. | map(.ssh_url) | @sh'  | tr -d '"' | tr -d "\'")
-#repos=$repos$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/orgs/mlab-modules/repos?page=6 | jq '. | map(.ssh_url) | @sh'  | tr -d '"' | tr -d "\'")
-#repos=$repos$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/orgs/mlab-modules/repos?page=7 | jq '. | map(.ssh_url) | @sh'  | tr -d '"' | tr -d "\'")
-
 repos=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/orgs/mlab-modules/repos?page=1 | jq '. | map(.ssh_url) | @sh'  | tr -d '"' | tr -d "\'")
 repos=$repos$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/orgs/mlab-modules/repos?page=2 | jq '. | map(.ssh_url) | @sh'  | tr -d '"' | tr -d "\'")
 sleep 2
@@ -52,12 +44,13 @@ repos=$repos$(curl -H "Accept: application/vnd.github.v3+json" https://api.githu
 
 
 
+echo $repos | wc -l
+
 echo " "
 echo "Budu stahovat repozitare:"
 echo $repos
 echo " "
 
 for i in $repos; do
-    echo "Repo " $i;
-    git clone $i --depth 2 --recurse-submodules;
+    echo $i;
 done
